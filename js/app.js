ac70795da94c7bf2f7a0d1bd48597450b839eebc
Asylkhan-Dauletbek <77191910+Asylkhan-Dauletbek@users.noreply.github.com>
2021-01-16 21:06:42 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
-let name = prompt("Как вас зовут?")
-alert("Привет " + name)
+// 1 задание начало
+
+/* let name = prompt("ваше имя?");
+alert(name); */
+
+// 1 задание конец
+
+// 2 задание начало
+
+// let number = 10;
+
+// nextPrime:
+// for (let i = 2; i <= number; i++) { // Для всех i...
+
+//    for (let check = 2; check < i; check++) { 
+//       if (i % check == 0) continue nextPrime; 
+//    }
+
+//    console.log(i);
+// }
+
+// 2 задание конец
